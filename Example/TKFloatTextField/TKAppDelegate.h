82aed8e26624311b2f206831b348086f62d39200
Duc Nguyen <duc.nguyen@tiki.vn>
2017-06-07 09:52:49 +0700

    Initial commit

---
+++ Example/TKFloatTextField/TKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TKAppDelegate.h
+//  TKFloatTextField
+//
+//  Created by duc.nguyen@tiki.vn on 06/07/2017.
+//  Copyright (c) 2017 duc.nguyen@tiki.vn. All rights reserved.
+//
+
+@import UIKit;
+
+@interface TKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
