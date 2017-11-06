6b62919f499f587ad04ef809fa434da41b78cf45
Duc Nguyen <duc.nguyen@tiki.vn>
2017-11-06 17:22:55 +0700

    Add new version FLoatTextField

--- Pod/Classes/ACFloatingTextField.h
+++ Pod/Classes/ACFloatingTextField.h
@@ -6,31 +6,65 @@
 //  Copyright © 2016 Abhishek. All rights reserved.
 //
 
+
 #import <UIKit/UIKit.h>
-@interface PlaceHolderLabel : UILabel
-@end
+
+IB_DESIGNABLE
 
 @interface ACFloatingTextField : UITextField
 {
     UIView *bottomLineView;
+    BOOL showingError;
 }
+/*
+ * Change the Bottom line color. Default is Black Color.
+ */
+@property (nonatomic,strong) IBInspectable UIColor *lineColor;
+/*
+ * Change the Placeholder text color. Default is Light Gray Color.
+ */
+@property (nonatomic,strong) IBInspectable UIColor *placeHolderColor;
+/*
+ * Change the Placeholder text color when selected. Default is [UIColor colorWithRed:19/256.0 green:141/256.0 blue:117/256.0 alpha:1.0].
+ */
+@property (nonatomic,strong) IBInspectable UIColor *selectedPlaceHolderColor;
+/*
+ * Change the bottom line color when selected. Default is [UIColor colorWithRed:19/256.0 green:141/256.0 blue:117/256.0 alpha:1.0].
+ */
+@property (nonatomic,strong) IBInspectable UIColor *selectedLineColor;
+/*
+ * Change the error label text color. Default is Red Color.
+ */
+@property (nonatomic,strong) IBInspectable UIColor *errorTextColor;
+/*
+ * Change the error line color. Default is Red Color.
+ */
+@property (nonatomic,strong) IBInspectable UIColor *errorLineColor;
+/*
+ * Change the error display text.
+ */
+@property (nonatomic,strong) IBInspectable  NSString  *errorText;
+/*
+ * Shake line when showing error?.
+ */
+@property (assign) IBInspectable  BOOL disableShakeWithError;
 
-@property (nonatomic,strong) UIColor *lineColor;
-@property (nonatomic,strong) UIColor *placeHolderColor;
-@property (nonatomic,strong) UIColor *selectedPlaceHolderColor;
-@property (nonatomic,strong) UIColor *selectedLineColor;
 
-@property (nonatomic,strong) PlaceHolderLabel *labelPlaceholder;
+@property (nonatomic,strong) UILabel *labelPlaceholder;
+@property (nonatomic,strong) UILabel *labelErrorPlaceholder;
 
-@property (assign) BOOL disableFloatingLabel;
-@property (strong, nonatomic) NSString *errorMessage;
-@property (assign, nonatomic) BOOL showError;
+
+@property (assign) IBInspectable  BOOL disableFloatingLabel;
+
+- (instancetype)init;
+- (instancetype)initWithFrame:(CGRect)frame;
+
+- (void)showErrorWithText:(NSString *)errorText;
+- (void)updateTextField:(CGRect)frame;
+
 @property (assign, nonatomic) CGFloat alignRight;
-
--(instancetype)init;
--(instancetype)initWithFrame:(CGRect)frame;
-
--(void)setTextFieldPlaceholderText:(NSString *)placeholderText;
--(void)updateTextField:(CGRect)frame;
+@property (assign, nonatomic) BOOL showError;
+@property (strong, nonatomic) NSString *errorMessage;
+- (void)setTextFieldPlaceholderText:(NSString *)placeholderText;
 
 @end
--- Pod/Classes/ACFloatingTextField.m
+++ Pod/Classes/ACFloatingTextField.m
@@ -10,15 +10,10 @@
 
 #define HEXC(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
-
-
-@implementation PlaceHolderLabel
-@end
-@interface ACFloatingTextField()
-@property (nonatomic, strong) UILabel *errorLabel;
-@end
+#import "ACFloatingTextField.h"
 
 @implementation ACFloatingTextField
+
 #pragma mark :- Drawing Methods
 - (void)drawRect:(CGRect)rect {
     [self updateTextField:CGRectMake(CGRectGetMinX(self.frame), CGRectGetMinY(self.frame), CGRectGetWidth(rect), CGRectGetHeight(rect))];
@@ -31,25 +26,17 @@
 }
 
 #pragma mark :- Initialization Methods
-- (instancetype)init {
-    self = [super init];
+- (instancetype) init {
     if (self) {
+        self = [super init];
         [self initialization];
     }
     return self;
 }
 
-- (instancetype)initWithFrame:(CGRect)frame {
-    self = [super initWithFrame:frame];
+-(instancetype)initWithFrame:(CGRect)frame {
     if (self) {
-        [self initialization];
-    }
-    return self;
-}
-
-- (instancetype)initWithCoder:(NSCoder *)aDecoder {
-    self = [super initWithCoder:aDecoder];
-    if (self) {
+        self = [super initWithFrame:frame];
         [self initialization];
     }
     return self;
@@ -70,21 +57,56 @@
 
 - (CGRect)clearButtonRectForBounds:(CGRect)bounds {
     CGRect originalRect = [super clearButtonRectForBounds:bounds];
-    return CGRectOffset(originalRect, -self.alignRight, 0);
+    return CGRectOffset(originalRect, - self.alignRight, 0);
 }
 
-#pragma mark:- Override Set text
+
+
+#pragma mark  Override Set text
+- (void)setShowError:(BOOL)showError {
+    showingError = showError;
+    if (showingError) {
+        [self beginShowError];
+    } else {
+        [self beginHideError];
+    }
+}
+
+- (BOOL)showError {
+    return showingError;
+}
+
 - (void)setText:(NSString *)text {
     [super setText:text];
-    [self floatTheLabel];
+    if (text) {
+        [self floatTheLabel];
+    }
+    if (showingError) {
+        [self hideErrorPlaceHolder];
+    }
+}
+
+#pragma mark  Set Placeholder Text On Label
+-(void)setPlaceholder:(NSString *)placeholder {
+    [super setPlaceholder:placeholder];
+    if (![placeholder isEqualToString:@""]) {
+        self.labelPlaceholder.text = placeholder;
+    }
+    
+}
+- (void)setTextFieldPlaceholderText:(NSString *)placeholderText {
+    [self setPlaceholder:placeholderText];
+}
+
+#pragma mark  Set Error Text On Label
+-(void)setErrorText:(NSString *)errorText {
+    _errorText = errorText;
+    self.labelErrorPlaceholder.text = errorText;
 }
 
 - (void)initialization {
-    
-    //HIDE DEFAULT PLACEHOLDER LABEL OF UITEXTFIELD
-    
-    [self checkForDefaulLabel];
-    
+    self.disableShakeWithError = YES;
+    self.clipsToBounds = NO;
     //VARIABLE INITIALIZATIONS
     
     //1. Placeholder Color.
@@ -107,12 +129,24 @@
         _selectedLineColor = HEXC(0x1BA8FF);
     }
     
+    
+    //5. Bottom line error Color.
+    if (_errorLineColor==nil) {
+        _errorLineColor = [UIColor redColor];
+    }
+    
+    //6. Bottom place Color When show error.
+    if (_errorTextColor==nil) {
+        _errorTextColor = [UIColor redColor];
+    }
+    
     /// Adding Bottom Line View.
     [self addBottomLineView];
     
     /// Adding Placeholder Label.
     [self addPlaceholderLabel];
     
+    [self setValue:self.placeHolderColor forKeyPath:@"_placeholderLabel.textColor"];
     
     /// Placeholder Label Configuration.
     if (![self.text isEqualToString:@""]) {
@@ -123,73 +157,102 @@
 
 #pragma mark :- Private Methods
 - (void)addBottomLineView{
+    if (bottomLineView.superview != nil) {
+        CGRect bottomLineFrame = bottomLineView.frame;
+        bottomLineFrame.origin.x = 2;
+        bottomLineFrame.size.width = self.frame.size.width - 2;
+        bottomLineView.frame = bottomLineFrame;
+        return;
+    }
     [bottomLineView removeFromSuperview];
-    bottomLineView = [[UIView alloc] initWithFrame:CGRectMake(2, CGRectGetHeight(self.frame)-5, CGRectGetWidth(self.frame) - 2, 1)];
+    
+    bottomLineView = [[UIView alloc] initWithFrame:CGRectMake(2, CGRectGetHeight(self.frame) - 1, CGRectGetWidth(self.frame) -2, 1)];
+    bottomLineView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
     bottomLineView.backgroundColor = _lineColor;
-    bottomLineView.tag = 20;
     [self addSubview:bottomLineView];
 }
 
-- (void)addPlaceholderLabel{
-    [_labelPlaceholder removeFromSuperview];
-    if (![self.placeholder isEqualToString:@""]&&self.placeholder!=nil) {
+- (void)addPlaceholderLabel {
+    if (![self.placeholder isEqualToString:@""] && self.placeholder!=nil) {
         _labelPlaceholder.text = self.placeholder;
     }
+    if (self.labelPlaceholder.superview != nil){
+        CGRect labelFrame = self.labelPlaceholder.frame;
+        labelFrame.origin.x = 5;
+        labelFrame.size.width = self.frame.size.width;
+        self.labelPlaceholder.frame = labelFrame;
+        return;
+    }
+    
+    [_labelPlaceholder removeFromSuperview];
     NSString *placeHolderText = _labelPlaceholder.text;
-    _labelPlaceholder = [[PlaceHolderLabel alloc] initWithFrame:CGRectMake(1, 0, self.frame.size.width-1, CGRectGetHeight(self.frame)+4)];
+    _labelPlaceholder = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, self.frame.size.width - 0, CGRectGetHeight(self.frame))];
     _labelPlaceholder.text = placeHolderText;
     _labelPlaceholder.textAlignment = self.textAlignment;
     _labelPlaceholder.textColor = _placeHolderColor;
     _labelPlaceholder.font = self.font;;
-    _labelPlaceholder.tag = 21;
+    _labelPlaceholder.hidden = YES;
     [self addSubview:_labelPlaceholder];
 }
 
-- (UILabel*)errorLabel{
-    if (_errorLabel == nil) {
-        _errorLabel = [[UILabel alloc]initWithFrame:CGRectMake(1, CGRectGetHeight(self.frame) + 4, self.frame.size.width-1, 14)];
-        _errorLabel.textAlignment = NSTextAlignmentLeft;;
-        _errorLabel.textColor = HEXC(0xe53935);
-        _errorLabel.font = [UIFont systemFontOfSize:12.f];
-        _errorLabel.tag = 22;
-        [self addSubview:_errorLabel];
-        _errorLabel.hidden = !self.showError;
-    }
-    _errorLabel.frame = CGRectMake(1, CGRectGetHeight(self.frame) + 4, self.frame.size.width-1, 14);
-    return _errorLabel;
+#pragma mark  Adding Error Label in textfield.
+- (void)addErrorPlaceholderLabel{
+    [self.labelErrorPlaceholder removeFromSuperview];
+    self.labelErrorPlaceholder = [[UILabel alloc] init];
+    self.labelErrorPlaceholder.text = self.errorText;
+    self.labelErrorPlaceholder.textAlignment = self.textAlignment;
+    self.labelErrorPlaceholder.textColor = self.errorTextColor;
+    self.labelErrorPlaceholder.font = [UIFont fontWithName:self.font.fontName size:12];
+    [self.labelErrorPlaceholder sizeToFit];
+    self.labelErrorPlaceholder.hidden = YES;
+    [self addSubview:self.labelErrorPlaceholder];
+    CGRect frameError = self.labelErrorPlaceholder.frame;
+    frameError.origin.x = 0;
+    frameError.origin.y = CGRectGetHeight(self.bounds) + 5;
+    self.labelErrorPlaceholder.frame = frameError;
 }
 
-/// Hadling The Default Placeholder Label
-- (void)checkForDefaulLabel{
-    if ([self.text isEqualToString:@""]) {
-        for (UIView *view in self.subviews) {
-            if ([view isKindOfClass:[PlaceHolderLabel class]]) {
-                
-                PlaceHolderLabel *newLabel = (PlaceHolderLabel *)view;
-                if (newLabel.tag!=21) {
-                    newLabel.hidden = YES;
-                }
-            }
-        }
+#pragma mark  Method to show Error Label.
+- (void) showErrorPlaceHolder{
+    CGRect bottmLineFrame = bottomLineView.frame;
+    bottmLineFrame.origin.y = self.frame.size.height - 1;
+    if (self.errorText != nil && ![self.errorText isEqualToString:@""]) {
+        [self addErrorPlaceholderLabel];
+        self.labelErrorPlaceholder.hidden = NO;
         
+        [UIView animateWithDuration:0.2 animations:^{
+            bottomLineView.frame  =  bottmLineFrame;
+            bottomLineView.backgroundColor = _errorLineColor;
+            self.labelErrorPlaceholder.alpha = 1;
+        }];
     } else {
-        
-        for (UIView *view in self.subviews) {
-            
-            if ([view isKindOfClass:[PlaceHolderLabel class]]) {
-                
-                PlaceHolderLabel *newLabel = (PlaceHolderLabel *)view;
-                if (newLabel.tag!=21) {
-                    newLabel.hidden = NO;
-                }
-            }
-        }
-        
+        [UIView animateWithDuration:0.2 animations:^{
+            bottomLineView.frame  =  bottmLineFrame;
+            bottomLineView.backgroundColor = _errorLineColor;
+        }];
     }
     
+    if (!self.disableShakeWithError) {
+        [self shakeView:bottomLineView];
+    }
 }
 
-#pragma mark  Upadate and Manage Subviews
+#pragma mark  Method to Hide Error Label.
+- (void)hideErrorPlaceHolder{
+    showingError = NO;
+    if (self.errorText == nil || [self.errorText isEqualToString:@""]) {
+        return;
+    }
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        self.
+        labelErrorPlaceholder.alpha = 0;
+    } completion:^(BOOL finished) {
+        [self.labelErrorPlaceholder removeFromSuperview];
+    }];
+}
+
+#pragma mark  Update and Manage Subviews
 - (void)updateTextField:(CGRect )frame {
     self.frame = frame;
     [self initialization];
@@ -197,63 +260,48 @@
 
 #pragma mark  Float UITextfield Placeholder Label.
 - (void)floatPlaceHolder:(BOOL)selected {
+    self.labelPlaceholder.hidden = NO;
+    CGRect bottmLineFrame = bottomLineView.frame;
     if (selected) {
-        bottomLineView.backgroundColor = _selectedLineColor;
-        
-        if (self.disableFloatingLabel) {
-            _labelPlaceholder.hidden = YES;
-            CGRect bottmLineFrame = bottomLineView.frame;
-            bottmLineFrame.origin.y = CGRectGetHeight(self.frame)-5;
-            [UIView animateWithDuration:0.2 animations:^{
-                bottomLineView.frame  =  bottmLineFrame;
-            }];
-            return;
-        }
-        
-        CGRect frame = _labelPlaceholder.frame;
-        frame.size.height = 12;
-        CGRect bottmLineFrame = bottomLineView.frame;
-        bottmLineFrame.origin.y = CGRectGetHeight(self.frame)-5;
-        [UIView animateWithDuration:0.2 animations:^{
-            _labelPlaceholder.frame = frame;
-            _labelPlaceholder.font = [UIFont fontWithName:self.font.fontName size:12];
-            _labelPlaceholder.textColor = _selectedPlaceHolderColor;
-            bottomLineView.frame  =  bottmLineFrame;
-            
-        }];
-        
+        bottomLineView.backgroundColor = self.selectedLineColor;
+        self.labelPlaceholder.textColor = self.selectedPlaceHolderColor;
+        bottmLineFrame.origin.y = CGRectGetHeight(self.frame) - 1;
+        [self setValue:self.selectedPlaceHolderColor forKeyPath:@"_placeholderLabel.textColor"];
     } else {
-        bottomLineView.backgroundColor = _lineColor;
-        if (self.disableFloatingLabel){
-            _labelPlaceholder.hidden = YES;
-            CGRect bottmLineFrame = bottomLineView.frame;
-            bottmLineFrame.origin.y = CGRectGetHeight(self.frame)-5;
-            [UIView animateWithDuration:0.2 animations:^{
-                bottomLineView.frame  =  bottmLineFrame;
-            }];
-            return;
-        }
-        
-        CGRect frame = _labelPlaceholder.frame;
-        frame.size.height = 12;
-        CGRect bottmLineFrame = bottomLineView.frame;
-        bottmLineFrame.origin.y = CGRectGetHeight(self.frame)-5;
+        bottomLineView.backgroundColor = self.lineColor;
+        self.labelPlaceholder.textColor = self.placeHolderColor;
+        bottmLineFrame.origin.y = CGRectGetHeight(self.frame)-1;
+        [self setValue:self.placeHolderColor forKeyPath:@"_placeholderLabel.textColor"];
+    }
+    if (self.disableFloatingLabel){
+        _labelPlaceholder.hidden = YES;
         [UIView animateWithDuration:0.2 animations:^{
-            _labelPlaceholder.frame = frame;
-            _labelPlaceholder.font = [UIFont fontWithName:self.font.fontName size:12];
-            _labelPlaceholder.textColor = _placeHolderColor;
             bottomLineView.frame  =  bottmLineFrame;
         }];
+        return;
     }
+    CGRect frame = self.labelPlaceholder.frame;
+    frame.size.height = 12;
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        _labelPlaceholder.frame = frame;
+        _labelPlaceholder.font = [UIFont fontWithName:self.font.fontName size:12];
+        bottomLineView.frame  =  bottmLineFrame;
+        
+    }];
+    
 }
 
-- (void)resignPlaceholder{
-    bottomLineView.backgroundColor = _lineColor;
-    if (self.disableFloatingLabel) {
-        _labelPlaceholder.hidden = NO;
-        _labelPlaceholder.textColor = _placeHolderColor;
-        CGRect bottmLineFrame = bottomLineView.frame;
-        bottmLineFrame.origin.y = CGRectGetHeight(self.frame)-5;
+- (void)resignPlaceholder {
+    [self setValue:self.placeHolderColor forKeyPath:@"_placeholderLabel.textColor"];
+    bottomLineView.backgroundColor = self.lineColor;
+    
+    CGRect bottmLineFrame = bottomLineView.frame;
+    bottmLineFrame.origin.y = CGRectGetHeight(self.frame) - 1;
+    
+    if (self.disableFloatingLabel){
+        self.labelPlaceholder.hidden = YES;
+        self.labelPlaceholder.textColor = self.placeHolderColor;
         [UIView animateWithDuration:0.2 animations:^{
             bottomLineView.frame  =  bottmLineFrame;
         }];
@@ -261,20 +309,29 @@
     }
     
     
-    CGRect frame = CGRectMake(2, 0, self.frame.size.width - 2, self.frame.size.height+4);
-    CGRect bottmLineFrame = bottomLineView.frame;
-    bottmLineFrame.origin.y = CGRectGetHeight(self.frame) - 5;
-    [UIView animateWithDuration:0.2 animations:^{
+    CGRect frame = CGRectMake(5, 0, self.frame.size.width-5, self.frame.size.height);
+    
+    [UIView animateWithDuration:0.3 animations:^{
         _labelPlaceholder.frame = frame;
         _labelPlaceholder.font = self.font;
         _labelPlaceholder.textColor = _placeHolderColor;
         bottomLineView.frame  =  bottmLineFrame;
+    } completion:^(BOOL finished) {
+        self.labelPlaceholder.hidden = YES;
+        self.placeholder = self.labelPlaceholder.text;
     }];
-
+    
 }
 #pragma mark  UITextField Begin Editing.
 
 - (void)textFieldDidBeginEditing {
+    if (showingError) {
+        [self hideErrorPlaceHolder];
+    }
+    if (!self.disableFloatingLabel) {
+        self.placeholder = @"";
+    }
+    
     [self floatTheLabel];
     [self layoutSubviews];
 }
@@ -286,63 +343,73 @@
 
 #pragma mark  Float & Resign
 
-- (void)floatTheLabel{
+-(void)floatTheLabel{
     
-    if ([self.text isEqualToString:@""]&&self.isFirstResponder) {
+    if ([self.text isEqualToString:@""] && self.isFirstResponder) {
         
         [self floatPlaceHolder:YES];
         
-    }else if ([self.text isEqualToString:@""]&&!self.isFirstResponder) {
-    
+    }else if ([self.text isEqualToString:@""] && !self.isFirstResponder) {
+        
         [self resignPlaceholder];
         
-    }else if (![self.text isEqualToString:@""]&&!self.isFirstResponder) {
+    }else if (![self.text isEqualToString:@""] && !self.isFirstResponder) {
         
         [self floatPlaceHolder:NO];
         
-    }else if (![self.text isEqualToString:@""]&&self.isFirstResponder) {
+    }else if (![self.text isEqualToString:@""] && self.isFirstResponder) {
         
         [self floatPlaceHolder:YES];
     }
-
-    [self checkForDefaulLabel];
-
-}
-
-
-#pragma mark  Set Placeholder Text On Labels
--(void)setTextFieldPlaceholderText:(NSString *)placeholderText {
-    
-    self.labelPlaceholder.text = placeholderText;
-    [self textFieldDidEndEditing];
-}
--(void)setPlaceholder:(NSString *)placeholder {
-    
-    self.labelPlaceholder.text = placeholder;
-    [self textFieldDidEndEditing];
     
 }
+#pragma mark  Shake Animation
+- (void)shakeView:(UIView*)view {
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"transform.translation.x"];
+    animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
+    animation.duration = 0.6;
+    animation.values = @[@(-20.0), @20.0, @(-20.0), @20.0, @(-10.0), @10.0, @(-5.0), @(5.0), @(0.0) ];
+    [view.layer addAnimation:animation forKey:@"shake"];
+}
+
+#pragma mark  Set Placeholder Text On Error Labels
+- (void)beginShowError {
+    showingError = YES;
+    [self showErrorPlaceHolder];
+}
+
+- (void)beginHideError {
+    showingError = false;
+    [self hideErrorPlaceHolder];
+    [self floatTheLabel];
+}
+
+- (void)showErrorWithText:(NSString *)errorText {
+    _errorText = errorText;
+    showingError = YES;
+    [self showErrorPlaceHolder];
+}
+
 #pragma mark  UITextField Responder Overide
 - (BOOL)becomeFirstResponder {
-
     BOOL result = [super becomeFirstResponder];
     [self textFieldDidBeginEditing];
     return result;
 }
 
--(BOOL)resignFirstResponder {
-
+- (BOOL) resignFirstResponder {
     BOOL result = [super resignFirstResponder];
     [self textFieldDidEndEditing];
     return result;
 }
+
 #pragma mark - Error Message
-- (void)setShowError:(BOOL)showError {
-    _showError = showError;
-    self.errorLabel.hidden = !showError;
-}
+
 - (void)setErrorMessage:(NSString *)errorMessage {
-    _errorMessage = errorMessage;
-    [self.errorLabel setText:errorMessage];
+    [self setErrorText:errorMessage];
+}
+
+- (NSString*)errorMessage {
+    return  self.errorText;
 }
 @end
